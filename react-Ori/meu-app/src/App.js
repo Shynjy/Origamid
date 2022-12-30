65d8a11ca48cb823c545c9ee03f098ed64a184cc
David <furia.designer@gmail.com>
2022-12-30 00:36:24 -0300

    Exercício 1 finalizado

--- react-Ori/meu-app/src/App.js
+++ react-Ori/meu-app/src/App.js
@@ -1,7 +1,66 @@
 import React from 'react';
 
-const App = () => {
-  return <div>Meu App</div>;
+
+// JSX 1
+// const App = () => {
+//   const nome = "David";
+
+//   return <>
+//     <p>{nome}</p>
+//   </>;
+// };
+
+// Exercício
+
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
 };
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+function somaTotal(dados) {
+  const valoresLimpos = dados.map((v) => Number(v.preco.replace("R$ ", "")));
+  return valoresLimpos.reduce((a, c) => a + c);
+}
+
+const App = () => {
+  const [dados, setDados] = React.useState(luana)
+
+  function changeName() {
+    if(dados.cliente !== 'Mario') {
+      setDados(mario)
+    } else {
+      setDados(luana)
+    }
+  }
+
+  return (  
+  <>
+    <p>None: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>Situação: <span style={dados.ativa ? {color: "green"} :{color: "red"} }>{dados.ativa ? "Ativo" : "Inativo" }</span></p>
+    <p>Total gastos: R$ {somaTotal(dados.compras)}</p>
+    <button onClick={changeName}>Mudar</button>
+  </>
+  );
+};
+
 
 export default App;
